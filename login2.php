7a0deb0c8602c679e66f8aa399529457b0ddd5d2
AryaAppaji <maappaji111@gmail.com>
2023-09-16 22:40:10 +0530

    Secured the Database credentials

--- login2.php
+++ login2.php
@@ -5,7 +5,7 @@ if(isset($_POST["mail"],$_POST["phno"])){
     $mail = $_POST["mail"];
     $phno = $_POST["phno"];
 }
-$conn = mysqli_connect("localhost","root","","blooddonation");
+$conn = $conn = mysqli_connect($_ENV["DB_HOST"],$_ENV["DB_USER_NAME"],$_ENV["DB_PASSWORD"],$_ENV["DB_NAME"]);
 $query = mysqli_query($conn,"SELECT * from `donars` WHERE `EMail`='{$mail}' and `Mobile` = '{$phno}'");
 $user = mysqli_fetch_assoc($query);
 
